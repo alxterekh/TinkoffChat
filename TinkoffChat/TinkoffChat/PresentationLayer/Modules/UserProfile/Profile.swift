68d1e4ed39560bce29850701207e59c006dbaaba
Alexander Terekhov <alexander.terekh@gmail.com>
2017-04-26 16:13:08 +0300

    shit

---
+++ TinkoffChat/TinkoffChat/PresentationLayer/Modules/UserProfile/Profile.swift
@@ -0,0 +1,42 @@
+//
+//  Profile.swift
+//  TinkoffChat
+//
+//  Created by Alexander on 30/03/2017.
+//  Copyright © 2017 Alexander Terekhov. All rights reserved.
+//
+
+import UIKit
+
+extension Profile: Equatable {}
+
+func ==(lhs: Profile, rhs: Profile) -> Bool {
+    return lhs.name == rhs.name &&
+        lhs.userInfo == rhs.userInfo &&
+        lhs.userPicture == rhs.userPicture
+}
+
+struct Profile {
+    let name: String
+    let userInfo: String
+    let userPicture: UIImage
+    
+    func hasDefaultUserPicture() -> Bool {
+        return self.userPicture == #imageLiteral(resourceName: "placeholder")
+    }
+    
+    static func createDefaultProfile() -> Profile {
+        let name = ""
+        let userInfo = "No user info."
+        return Profile(name: name, userInfo: userInfo, userPicture: #imageLiteral(resourceName: "placeholder"))
+    }
+    
+    func createCopyWithChange(name: String? = nil, userInfo: String? = nil, userPicture: UIImage? = nil) -> Profile {
+        
+        let copyName = name ?? self.name
+        let copyUserInfo = userInfo ?? self.userInfo
+        let copyUserPicture = userPicture ?? self.userPicture
+        
+        return Profile(name: copyName, userInfo: copyUserInfo, userPicture: copyUserPicture)
+    }
+}
---
+++ TinkoffChat/TinkoffChat/PresentationLayer/Modules/UserProfile/ProfileEditorViewController.swift
@@ -0,0 +1,231 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by Alexander on 03/03/2017.
+//  Copyright © 2017 Alexander Terekhov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileEditorViewController: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet fileprivate weak var usernameField: UITextField!
+    @IBOutlet fileprivate weak var userinfoText: UITextView!
+    @IBOutlet fileprivate weak var avatarImageView: UIImageView!
+    @IBOutlet fileprivate weak var saveProfileByGCDButton: UIButton!
+    @IBOutlet fileprivate weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate lazy var photoPicker : UIImagePickerController = {
+        return UIImagePickerController()
+    }()
+    
+    fileprivate let profileDataService = ServiceAssembly.profileDataService()
+    
+    fileprivate var originalProfile = Profile.createDefaultProfile()
+    fileprivate var changedProfile = Profile.createDefaultProfile() {
+        didSet {
+            updateView()
+        }
+    }
+    
+    var profile: Profile { return changedProfile }
+    
+    // MARK: - Actions
+    
+    @IBAction fileprivate func saveProfileDataByGCDButtonTap(_ sender: UIButton) {
+        if (!(changedProfile == originalProfile)) {
+            saveProfile()
+        }
+        else {
+            self.performSegue(withIdentifier: "unwindToConversationList", sender: self)
+        }
+    }
+    
+    // MARK: - Initialization
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    fileprivate func setup() {
+        setupDependencies()
+        setupGestureRecognizer()
+        setDefaultUserNamePlaceholder()
+        loadProfileData()
+    }
+    
+    fileprivate func setupDependencies() {
+        usernameField.delegate = self
+        userinfoText.delegate = self
+        photoPicker.delegate = self
+    }
+    
+    fileprivate func setupGestureRecognizer() {
+        let tapToDismissKeyboards = UITapGestureRecognizer(target: self, action: #selector(ProfileEditorViewController.dismissKeyboard))
+        view.addGestureRecognizer(tapToDismissKeyboards)
+        
+        let tapOnImage = UITapGestureRecognizer(target: self, action: #selector(ProfileEditorViewController.imageTapped(tapGestureRecognizer:)))
+        avatarImageView.isUserInteractionEnabled = true
+        avatarImageView.addGestureRecognizer(tapOnImage)
+    }
+    
+    fileprivate func setDefaultUserNamePlaceholder() {
+        usernameField.placeholder = UIDevice.current.name
+    }
+    
+    // MARK: - Alerts
+    
+    fileprivate func showSucceesfulDataSaveOperationAlert() {
+        let alert = UIAlertController(title: "Данные сохранены", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) {
+            [unowned self] action in
+            self.performSegue(withIdentifier: "unwindToConversationList", sender: self)
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    fileprivate func showFailedDataSaveOperationAlert() {
+        let alert = UIAlertController(title: "Ошибка", message: "Не удалось сохранить данные", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) {
+            [unowned self] action in
+            self.performSegue(withIdentifier: "unwindToConversationList", sender: self)
+        })
+        alert.addAction(UIAlertAction(title: "Повторить", style: .default) {
+            [unowned self] action in
+            self.saveProfile()
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: -
+    
+    fileprivate func updateView() {
+        usernameField.text = changedProfile.name
+        userinfoText.text = changedProfile.userInfo
+        avatarImageView.image = changedProfile.userPicture
+    }
+    
+    fileprivate func loadProfileData() {
+        activityIndicator.startAnimating()
+        profileDataService.loadProfileData() {
+            self.activityIndicator.stopAnimating()
+            if let profile = $0 {
+                self.originalProfile = profile
+                self.changedProfile = profile
+            }
+            else {
+                self.changedProfile = Profile.createDefaultProfile()
+            }
+            
+            self.handleDataOperationError($1)
+        }
+    }
+    
+    fileprivate func saveProfile() {
+        activityIndicator.startAnimating()
+        saveProfileByGCDButton.isEnabled = false
+        profileDataService.saveProfileData(changedProfile) {
+            success, error in
+            
+            self.activityIndicator.stopAnimating()
+            if success {
+                self.showSucceesfulDataSaveOperationAlert()
+                self.originalProfile = self.changedProfile
+            }
+            else {
+                self.showFailedDataSaveOperationAlert()
+            }
+            
+            self.saveProfileByGCDButton.isEnabled = true
+            self.handleDataOperationError(error)
+        }
+    }
+    
+    fileprivate func handleDataOperationError(_ error: Error?) {
+        if let savingError = error {
+            print("\(savingError)")
+        }
+    }
+    
+    // MARK: -
+    
+    @objc fileprivate func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        let userPictureActionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        addDefaultActionsToSheet(userPictureActionSheet)
+        if (!changedProfile.hasDefaultUserPicture()) {
+            addDeleteActionToSheet(userPictureActionSheet)
+        }
+        present(userPictureActionSheet, animated: true)
+    }
+    
+    fileprivate func addDefaultActionsToSheet(_ sheet: UIAlertController) {
+        sheet.addAction(UIAlertAction(title: "New photo", style: .default) {
+            [unowned self] action in
+            
+            self.photoPicker.allowsEditing = false
+            self.photoPicker.sourceType = UIImagePickerControllerSourceType.camera
+            self.photoPicker.cameraCaptureMode = .photo
+            self.photoPicker.modalPresentationStyle = .fullScreen
+            self.present(self.photoPicker, animated: true)
+        })
+        
+        sheet.addAction(UIAlertAction(title: "Select photo", style: .default) {
+            [unowned self] action in
+            
+            self.photoPicker.allowsEditing = false
+            self.photoPicker.sourceType = .photoLibrary
+            self.photoPicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+            self.present(self.photoPicker, animated: true)
+        })
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel) { action in })
+    }
+    
+    fileprivate func addDeleteActionToSheet(_ sheet: UIAlertController) {
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive) {
+            [unowned self] action in
+            
+            self.avatarImageView.image = #imageLiteral(resourceName: "placeholder")
+            self.changedProfile = self.changedProfile.createCopyWithChange(userPicture: #imageLiteral(resourceName: "placeholder"))
+        })
+    }
+    
+    @objc fileprivate func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension ProfileEditorViewController : UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        changedProfile = changedProfile.createCopyWithChange(userPicture: chosenImage)
+        dismiss(animated:true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ProfileEditorViewController : UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if let text = textView.text {
+            changedProfile = changedProfile.createCopyWithChange(userInfo: text)
+        }
+    }
+}
+
+extension ProfileEditorViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let text = textField.text {
+            changedProfile = changedProfile.createCopyWithChange(name: text)
+        }
+    }
+}
